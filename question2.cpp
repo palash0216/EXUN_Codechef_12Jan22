904b704bbad4ff3aeacce9fcfeb397e90a5fb0da
palash mishra <75239216+palash0216@users.noreply.github.com>
2022-01-12 22:55:11 +0530

    Add files via upload

---
+++ question2.cpp
@@ -0,0 +1,46 @@
+/*
+Sarthak and his Magic Grid Problem Code: GRIDXOR
+Add problem to Todo list
+Submit
+Sarthak got an empty magic grid of size N×N on his birthday. The grid can only be filled with positive integers such that the bitwise XOR of all numbers from any row, any column, or any of the two main diagonals must all be the same! Can you help Sarthak fill this grid using only positive integers no more than 109?
+
+Input Format
+The first line of each input contains T - the number of test cases. The test cases then follow.
+The only line of each test case contains an integer N - the size of the grid.
+Output Format
+For each test case, output N lines, each containing N space-separated integers, such that the grid satisfies the condition given in the statement.
+
+Each integer should be positive and ≤109.
+
+Constraints
+1≤T≤100
+1≤N≤100
+Sample Input 1 
+1
+3
+Sample Output 1 
+1 1 1
+1 1 1
+1 1 1
+Explanation
+The bitwise XOR of all rows, columns and main diagonals in the grid is 1. Therefore, this satisfies the condition.*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t, N;
+    cin >> t;
+    while (t--)
+    {
+        cin >> N;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                cout << 1 << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
